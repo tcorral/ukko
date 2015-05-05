f7f261e50f9c965d9920df1ddf540edb1d7b58ad
Tomas Corral <tomas@backbase.com>
2015-05-05 17:47:44 +0200

    Let the task as a linker it could be improved in next releases to perform the download of the repos.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var bower = require('bower');
 var path = require('path');
 var async = require('async');
-var Bower = require('./lib/bower');
 var utils = require('./lib/utils');
-var Commands = require('./lib/commands');
 var validLink = require('./lib/utils/checkLink');
 var createLink = require('./lib/utils/createLink');
 
@@ -13,125 +10,34 @@ var installOrUpdate = function (config) {
     }
     var configPath = config.configPath;
     var repos = config.repos;
-    var force = config.force;
     var onEnd = config.onEnd || function () {};
     var detachedProcesses = {processes: []};
     var confEnvironment = utils.getConfigEnvironment(configPath);
     var lastProcess = {_process: null};
-    var correctCallbacks = [];
-    var postCallbacks = [];
     var endPointKeys = utils.getRepos(repos, confEnvironment);
     require('./lib/safe-exit')(detachedProcesses, lastProcess);
-    // Clean bower cache. - avoid bower to remove .git folder.
-    bower.commands.cache.clean()
-        .on('log', function (data) {
-            var log = data && data.level && console[data.level] ? console[data.level] : console.log;
-            log('....', data.message);
-        })
-        .on('error', function () {
-            console.warn('error', arguments);
-        })
-        .on('end', function () {
-            console.log('Cache cleaned');
-            async.eachSeries(endPointKeys, function (key, callback) {
-                // Execute preinstall
-                Commands.execute({
-                    cwd: process.cwd(),
-                    key: key,
-                    detachedProcesses: detachedProcesses,
-                    lastProcess: lastProcess,
-                    commands: Commands.getPreCommands(confEnvironment[key]),
-                    callback: function () {
-                        var argumentsToBower = [];
-                        var _path = path.resolve(process.cwd(), key);
-                        var command;
-                        var originalKey = key;
-                        var settingsObj = {save: false};
-                        var optionsObj = {
-                            interactive: false,
-                            directory: key
-                        };
-                        if (force) {
-                            optionsObj["force-latest"] = true;
-                        }
-                        if (utils.isString(confEnvironment[key])) {
-                            argumentsToBower.push([confEnvironment[key]], settingsObj, optionsObj);
-                        } else if (utils.isObject(confEnvironment[key])) {
-                            argumentsToBower.push([confEnvironment[key].endpoint], settingsObj, optionsObj);
-                        }
-                        command = Bower.getBowerCommand(key, argumentsToBower);
 
-                        console.log(key, validLink(path.resolve(process.cwd(), key)));
+    async.eachSeries(endPointKeys, function (key, callback){
+        var source = path.resolve(process.cwd(), (confEnvironment[key].endpoint || confEnvironment[key]));
+        var target = path.resolve(process.cwd(), key);
+        validLink(target)
+            .then(function (result){
+                console.log('source:' + source, 'target:' + target);
+                if(result[0] === false){
+                    createLink(source, target).then(function (){
+                        callback();
+                    });
+                }else{
+                    callback();
+                }
 
-                        // check if the symbolink link exist. If it exist then it's not needed to do anything with ukko.
-                        validLink(path.resolve(process.cwd(), key))
-                            .then(function (result) {
-                                if(result[0] === false){
-                                    if (command === 'update') {
-                                        key = key.replace(/\//g, '__');
-                                        Bower.saveToUpdate(key);
-                                    }
-
-                                    bower.commands[command].apply(bower, argumentsToBower)
-                                        .on('log', function (data) {
-                                            var log = data && data.level && console[data.level] ? console[data.level] : console.log;
-                                            log(data.message);
-                                        })
-                                        .on('error', function () {
-                                            console.warn('error', arguments);
-                                        })
-                                        .on('end', function (installed) {
-                                            var userHome = utils.getUserHome();
-                                            if (command === 'install') {
-                                                Bower.saveDependencies(installed, key);
-                                            }
-                                            Bower.cleanDependencies();
-
-                                            correctCallbacks.push((function (installed, _path) {
-                                                return function (_callback) {
-                                                    utils.correctParentFolder(installed, _path, function () {
-                                                        Bower.cleanDependencies();
-                                                    });
-                                                    _callback();
-                                                };
-                                            }(installed, _path)));
-
-                                            postCallbacks.push.apply(postCallbacks, (function (cwd, key) {
-                                                return Commands.getCommandsCallbacks({
-                                                    cwd: cwd,
-                                                    detachedProcesses: detachedProcesses,
-                                                    lastProcess: lastProcess,
-                                                    commands: Commands.getPostCommands(confEnvironment[key]),
-                                                    callback: function (_callback) {
-                                                        _callback();
-                                                    }
-                                                });
-                                            }(_path, originalKey)));
-
-                                            // If there is no link we link it as private and as global links.
-                                            console.log('Creating link src:' + originalKey + 'dest:' + path.basename(originalKey));
-                                            createLink(path.basename(originalKey), path.join(userHome, '.local', 'share', 'bower', 'links', originalKey));
-
-                                            // Follow next step.
-                                            callback();
-                                        });
-                                }
-                            });
-
-                    }
-                });
-            }, function () {
-                async.series(correctCallbacks, function () {
-                    async.series(postCallbacks, function () {
-                        if(!utils.getSaveReportFlag(configPath)){
-                            fs.unlinkSync(path.join(cwd,'ukko-output.log'));
-                            fs.unlinkSync(path.join(cwd,'ukko-error.log'));
-                        }
-                        onEnd();
-                    })
-                });
             });
-        });
+    }, function (err){
+        if(err){
+            throw err;
+        }
+        onEnd();
+    });
 };
 
 module.exports = {
