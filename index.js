ab349e1c826f0990ed291fc9b4834a969713bdf5
Tomas Corral <tomas@backbase.com>
2015-05-11 14:38:56 +0200

    - Add support to check for install or update

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 var path = require('path');
+var fs = require('fs');
 var async = require('async');
 var utils = require('./lib/utils');
 var mystiquex = require('mystiquex');
+var errors = require('./lib/errors');
 
 var installOrUpdate = function (config) {
     if (!config) {
-        throw new Error('Config object is needed to make ukko work as expected. Please review your code and fix it.');
+        console.log(errors.configObjectMissing);
+        throw new Error(errors.configObjectMissing);
     }
-    var configPath = config.configPath;
     var repos = config.repos;
     var onEnd = config.onEnd || function () {};
-    var confEnvironment = utils.getConfigEnvironment(configPath || config.data);
+    var confEnvironment = utils.getConfigEnvironment(utils.getConfig(config));
     var endPointKeys = utils.getRepos(repos, confEnvironment);
 
+    if (!confEnvironment || endPointKeys.length === 0) {
+        console.log(errors.wrongConfig);
+        throw new Error(errors.wrongConfig);
+    }
+
     async.eachSeries(endPointKeys, function (key, callback){
+        console.log('Checking ' + key + ' endpoint');
         var source = (confEnvironment[key].endpoint || confEnvironment[key]);
-        var decEndpoint = { source: source, target: key, commands: (confEnvironment[key].commands || {}) };
-        var detachedProcesses = utils.getDetachedProcesses(configPath || config.data);
-        var saveReports = utils.getSaveReportFlag(configPath || config.data);
-        var commandsLength = utils.getCommandsLength(configPath || config.data);
+        var decEndpoint = { source: source, target: key, commands: (confEnvironment[key].commands || {}), "testAuth": confEnvironment[key].testAuth };
+        var detachedProcesses = utils.getDetachedProcesses(utils.getConfig(config));
+        var saveReports = utils.getSaveReportFlag(utils.getConfig(config));
+        var commandsLength = utils.getCommandsLength(utils.getConfig(config));
+        var target = path.resolve(process.cwd(), key);
         mystiquex.getResolver(source, decEndpoint)
             .then(function (data) {
                 var resolverInstance = new data.resolver(data.endpoint);
